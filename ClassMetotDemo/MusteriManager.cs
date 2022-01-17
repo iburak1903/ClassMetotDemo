574a974c8ff2a73e9cce4c619c62ac04323f1792
iburak1903 <97766791+iburak1903@users.noreply.github.com>
2022-01-17 22:25:38 +0300

    ClassMetotDemo

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class MusteriManager
+    {
+       public void Ekleme(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri listeye eklendi: " + musteri.AdSoyad + " -"+" Müşteri Yaş: " + musteri.Yas +" -" +" Müşteri Id:" + musteri.Id);
+
+        }
+        public void Silme(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri listeden silindi:" + musteri.AdSoyad + " -" + " Müşteri Yaş : " + musteri.Yas + " -" + " Müşteri Id: " + musteri.Id);
+        }
+        public void Listeleme(Musteri[] musteriler)
+        {
+            foreach (var müsteri in musteriler)
+            {
+                Console.WriteLine(" Müşteri Adı: "+müsteri.AdSoyad + " -"+ " Müşteri Yaş: " +  müsteri.Yas+ " -"+" Müşteri Id: " + müsteri.Id);
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿//Musteri isimli bir Class oluşturunuz. Müşteriye istediğiniz özellikleri ekleyiniz. (Id,Ad,Soyad....)
+//MusteriManager sınıfı oluşturunuz. Musteri parametresi alarak Musteri ekleme, listeleme, silme gibi metotları simule ediniz.
+
+ClassMetotDemo.Musteri musteri1 = new ClassMetotDemo.Musteri();
+musteri1.AdSoyad = "Mazlum Altan";
+musteri1.Yas = 20;
+musteri1.Id = 185;
+
+ClassMetotDemo.Musteri musteri2 = new ClassMetotDemo.Musteri();
+musteri2.AdSoyad = "Şükrü Arap";
+musteri2.Yas = 45;
+musteri2.Id = 201;
+
+ClassMetotDemo.Musteri musteri3 = new ClassMetotDemo.Musteri();
+musteri3.AdSoyad = "Ateş Aycı";
+musteri3.Yas = 27;
+musteri3.Id = 312;
+
+ClassMetotDemo.Musteri musteri4 = new ClassMetotDemo.Musteri();
+musteri4.AdSoyad = "Ali İsmail Babacan";
+musteri4.Yas = 35;
+musteri4.Id = 145;
+
+ClassMetotDemo.Musteri[] müsteriler = new ClassMetotDemo.Musteri[] {musteri1, musteri2, musteri4,};
+
+ClassMetotDemo.MusteriManager musteriManager = new ClassMetotDemo.MusteriManager();
+musteriManager.Ekleme(musteri4);
+
+Console.WriteLine("-");
+
+ClassMetotDemo.MusteriManager musteriManager2 = new ClassMetotDemo.MusteriManager();
+musteriManager2.Silme(musteri2);
+
+Console.WriteLine("-----Müşteri Listesi:------");
+
+ClassMetotDemo.MusteriManager musteriManager3 = new ClassMetotDemo.MusteriManager();
+musteriManager3.Listeleme(müsteriler);
+
+
+
